f7e4d8968643d7b37dab451e18319bb7086a4261
Jack Michell <jmichell2003@gmail.com>
2025-03-12 13:37:09 -0500

    added getSmallestValue function

--- main.c
+++ main.c
@@ -24,6 +24,19 @@ int getLargestValue(int arr[rows][cols]) {
     return largest;
 }
 
+int getSmallestValue(int arr[rows][cols]) {
+    // Compare with largest value to get smallest
+    int smallest = getLargestValue(arr);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (smallest > arr[i][j]) {
+                smallest = arr[i][j];
+            }
+        }
+    }
+    return smallest;
+}
+
 Position getPosition(int arr[rows][cols], int target) {
     Position position;
     for (int i = 0; i < rows; i++) {
@@ -39,22 +52,20 @@ Position getPosition(int arr[rows][cols], int target) {
 }
 
 void printArray(int arr[rows][cols]) {
-    printf("\n---------------\n");
+    printf("\n");
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
-    printf("---------------\n");
 }
-
 void fillArray(int arr[rows][cols]) {
     int min, max;
-
     printf("Enter range of numbers (min max): ");
     scanf("%d %d", &min, &max);
 
+    srand(time(NULL));
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             arr[i][j] = generateNumber(min, max);
@@ -65,15 +76,19 @@ void fillArray(int arr[rows][cols]) {
 int main() {
     printf("Enter the size of the 2D array you want: ");
     scanf("%d %d", &rows, &cols);
-    int arr[rows][cols];
 
-    srand(time(NULL));
+    int arr[rows][cols];
     fillArray(arr);
     printArray(arr);
+
     int largest = getLargestValue(arr);
-    Position position = getPosition(arr, largest);
-    printf("\nLargest Value: %d | Position: [%d][%d]\n", largest, position.x,
-           position.y);
+    int smallest = getSmallestValue(arr);
+    Position largestPosition = getPosition(arr, largest);
+    Position smallestPosition = getPosition(arr, smallest);
+    printf("\nSmallest Value: %d | Position: [%d][%d]\n", smallest,
+           smallestPosition.x, smallestPosition.y);
+    printf("Largest Value: %d | Position: [%d][%d]\n", largest,
+           largestPosition.x, largestPosition.y);
 
     return 0;
 }
